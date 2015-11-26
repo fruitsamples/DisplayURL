aa5f87823f16f0db972e7dc96b9986a94d818d47
Steve Moser <contact@stevemoser.org>
2015-11-25 20:34:45 -0500

    Add OSX SDK 10.6 (409.200) Docset changes

--- main.c
+++ main.c
@@ -1,61 +1,63 @@
 /*
-	File:		main.c
-
-	Abstract:	DisplayURL creates a CFURL and then uses CFURL functions to parse
-				the CFURL and display its components. This is very useful for
-				determining the exact behavior of the CFURL functions. DisplayURL
-				accepts either a URL or path (from which a URL is created).
-				DisplayURL allows you to create relative CFURLs and leave them
-				relative, or make them absolute before parsing. And DisplayURL
-				allows you to use the delete/append component and extension functions.
-				
-				Examples:
-					DisplayURL "scheme://user:pass@host:1/path/path2/file.html;params?query#fragment"
-					DisplayURL -P "/System/Library/"
-
-				Note: for the CFURL parsing routines to work correctly, any URL
-				strings passed must conform to IETF rfc3986 <www.ietf.org/rfc/rfc3986.txt>.
-				That means reserved characters must be properly escaped.
-
-	Version:	2.0
-
-	Disclaimer:	IMPORTANT:  This Apple software is supplied to you by Apple Computer, Inc.
-				("Apple") in consideration of your agreement to the following terms, and your
-				use, installation, modification or redistribution of this Apple software
-				constitutes acceptance of these terms.  If you do not agree with these terms,
-				please do not use, install, modify or redistribute this Apple software.
-
-				In consideration of your agreement to abide by the following terms, and subject
-				to these terms, Apple grants you a personal, non-exclusive license, under Apple's
-				copyrights in this original Apple software (the "Apple Software"), to use,
-				reproduce, modify and redistribute the Apple Software, with or without
-				modifications, in source and/or binary forms; provided that if you redistribute
-				the Apple Software in its entirety and without modifications, you must retain
-				this notice and the following text and disclaimers in all such redistributions of
-				the Apple Software.  Neither the name, trademarks, service marks or logos of
-				Apple Computer, Inc. may be used to endorse or promote products derived from the
-				Apple Software without specific prior written permission from Apple.  Except as
-				expressly stated in this notice, no other rights or licenses, express or implied,
-				are granted by Apple herein, including but not limited to any patent rights that
-				may be infringed by your derivative works or by other works in which the Apple
-				Software may be incorporated.
-
-				The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO
-				WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED
-				WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-				PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION ALONE OR IN
-				COMBINATION WITH YOUR PRODUCTS.
-
-				IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR
-				CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
-				GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-				ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, MODIFICATION AND/OR DISTRIBUTION
-				OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER UNDER THEORY OF CONTRACT, TORT
-				(INCLUDING NEGLIGENCE), STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN
-				ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-	Copyright 2005-2009 Apple Computer, Inc., All Rights Reserved
-*/
+     File: main.c
+ Abstract: DisplayURL creates a CFURL and then uses CFURL functions to parse
+ the CFURL and display its components. This is very useful for
+ determining the exact behavior of the CFURL functions. DisplayURL
+ accepts either a URL or path (from which a URL is created).
+ DisplayURL allows you to create relative CFURLs and leave them
+ relative, or make them absolute before parsing. And DisplayURL
+ allows you to use the delete/append component and extension functions.
+ 
+ Examples:
+ DisplayURL "scheme://user:pass@host:1/path/path2/file.html;params?query#fragment"
+ DisplayURL -P "/System/Library/"
+ 
+ Note: for the CFURL parsing routines to work correctly, any URL
+ strings passed must conform to IETF rfc3986 www.ietf.org/rfc/rfc3986.txt.
+ That means reserved characters must be properly escaped.
+  Version: 2.1
+ 
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
+ Inc. ("Apple") in consideration of your agreement to the following
+ terms, and your use, installation, modification or redistribution of
+ this Apple software constitutes acceptance of these terms.  If you do
+ not agree with these terms, please do not use, install, modify or
+ redistribute this Apple software.
+ 
+ In consideration of your agreement to abide by the following terms, and
+ subject to these terms, Apple grants you a personal, non-exclusive
+ license, under Apple's copyrights in this original Apple software (the
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple
+ Software, with or without modifications, in source and/or binary forms;
+ provided that if you redistribute the Apple Software in its entirety and
+ without modifications, you must retain this notice and the following
+ text and disclaimers in all such redistributions of the Apple Software.
+ Neither the name, trademarks, service marks or logos of Apple Inc. may
+ be used to endorse or promote products derived from the Apple Software
+ without specific prior written permission from Apple.  Except as
+ expressly stated in this notice, no other rights or licenses, express or
+ implied, are granted by Apple herein, including but not limited to any
+ patent rights that may be infringed by your derivative works or by other
+ works in which the Apple Software may be incorporated.
+ 
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+ 
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ 
+ Copyright (C) 2011 Apple Inc. All Rights Reserved.
+ 
+ */
 
 #define DEBUG 1
 #include <unistd.h>
@@ -328,6 +330,7 @@ int main (int argc, char * const argv[])
 				}
 				break;
 			case 'b':
+				CF_RELEASE_CLEAR(baseURLString);
 				baseURLString = CFStringCreateWithBytes(kCFAllocatorDefault, (UInt8 *)optarg, strlen(optarg), kCFStringEncodingUTF8, FALSE);
 				require_action(baseURLString != NULL, CFStringCreateWithBytes_baseURLString, err = EXIT_FAILURE);
 				break;
@@ -339,6 +342,7 @@ int main (int argc, char * const argv[])
 				break;
 			case 'X':
 				appendPathExtension = TRUE;
+				CF_RELEASE_CLEAR(pathExtension);
 				pathExtension = CFStringCreateWithBytes(kCFAllocatorDefault, (UInt8 *)optarg, strlen(optarg), kCFStringEncodingUTF8, FALSE);
 				require_action(pathExtension != NULL, CFStringCreateWithBytes_pathExtension, err = EXIT_FAILURE);
 				break;
@@ -347,6 +351,7 @@ int main (int argc, char * const argv[])
 				break;
 			case 'C':
 				appendPathComponent = TRUE;
+				CF_RELEASE_CLEAR(lastPathComponent);
 				lastPathComponent = CFStringCreateWithBytes(kCFAllocatorDefault, (UInt8 *)optarg, strlen(optarg), kCFStringEncodingUTF8, FALSE);
 				require_action(lastPathComponent != NULL, CFStringCreateWithBytes_lastPathComponent, err = EXIT_FAILURE);
 				break;
